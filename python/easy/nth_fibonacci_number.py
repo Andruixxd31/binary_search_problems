8699bfd3d41727c628d07fa044cdb1d8a5c3a8c8
Andruixxd31 <andruix93@gmail.com>
2022-03-14 12:59:38 -0600

    solved

---
+++ python/easy/nth_fibonacci_number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def solve(self, n):
+        l = [0] * n
+        for i in range(min(2,n)):
+            l[i] = 1
+        
+        for i in range(2, n):
+            l[i] = l[i-1] + l[i-2]
+       
+        return(l[n-1]) if n > 0 else 1
